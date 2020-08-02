d0d0791085262c881b19b775def4a7ebb831f428
Sergey Semenov <dindrophone@gmail.com>
2020-08-02 20:52:49 +0300

    add card-hero factoid template

---
+++ components/@atoms/Factoid.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="factoid">
+    <p class="factoid__top" v-if="factoid.top">{{ factoid.top }}</p>
+    <div class="factoid__main">
+      <p class="factoid__before" v-if="factoid.before">{{ factoid.before }}</p>
+      <p class="factoid__number">{{ factoid.text }}</p>
+      <p class="factoid__after" v-if="factoid.after">{{ factoid.after }}</p>
+      <div class="factoid__labels" v-if="factoid.sub || factoid.sup">
+        <p class="factoid__sup" v-if="factoid.sup">{{ factoid.sup }}</p>
+        <p class="factoid__sub" v-if="factoid.sub">{{ factoid.sub }}</p>
+      </div>
+    </div>
+    <p class="factoid__title" v-if="factoid.title">{{ factoid.title }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/^molecules/CardHero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card-hero">
+    <div class="card-hero__bg"></div>
+    <div class="container">
+      <div class="card-hero__content">
+        <h1 v-if="card.title" class="card-hero__title h1">{{ card.title }}</h1>
+        <actions v-if="card.actions" class="card-hero__actions" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
