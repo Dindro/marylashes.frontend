1a0a63c92e3b36d9b5cdcc945a5dcef78ca48991
Sergey Semenov <dindrophone@gmail.com>
2020-08-07 08:15:36 +0300

    Исправил container добавил image компонент

---
+++ components/+atoms/ImageAdaptive.vue
@@ -0,0 +1,53 @@
+<template>
+  <component
+    class="image"
+    :is="is"
+    :src="image.src"
+    :srcset="srcset"
+    :alt="alt"
+    :title="image.title"
+  >
+  </component>
+</template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        is: this.image.type === 'bg' ? 'div' : 'img',
+        alt: this.image.alt || this.image.caption || 'image',
+      }
+    },
+
+    computed: {
+      srcset() {
+        if (!Array.isArray(this.image.srcset)) return;
+
+        let srcset = '';
+        this.image.srcset.forEach((item, index) => {
+          srcset += item.src;
+          if (item.scale && item.scale > 1) {
+            srcset += item.scale;
+            srcset += 'x';
+          }
+          if (index < this.image.srcset.lenght - 1) {
+            srcset += ', ';
+          }
+        });
+
+        return srcset;
+      },
+    },
+
+    props: {
+      image: {
+        type: Object,
+        required: true,
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/^molecules/CardService.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card-service">
+    <div class="card-service__illustration"></div>
+
+    <div class="card-service__content">
+      <div class="card-service__header">
+        <h3 class="card-service__title">
+          <span class="card-service__name">{{ card.name }}</span>
+            —
+          <span class="card-service__price">{{ card.price }}</span>
+        </h3>
+        <p class="card-service__text" v-html="card.text"></p>
+      </div>
+      <div class="card-service__actions">
+        <actions :actions="card.actions"></actions>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Actions from '^/Actions';
+  export default {
+    components: {
+      Actions,
+    },
+    // TODO: Global Price Filter
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
