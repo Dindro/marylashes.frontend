39b8c44238769b336aec38fc913d77e00593bf1c
Sergey Semenov <dindro@MacBook-Pro-Sergey.local>
2020-12-05 17:01:36 +0300

    Добавил спиннер блок

---
+++ components/&organisms/SpinnerBlock.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="spinner-block">
+		<spinner class="spinner-block__element"/>
+	</div>
+</template>
+
+<script>
+import Spinner from '+/Spinner';
+
+export default {
+	components: {
+		Spinner,
+	},
+}
+</script>
+
+<style lang="scss">
+.spinner-block {
+	display: flex;
+	height: 100%;
+
+	&__element {
+		margin: auto;
+	}
+}
+</style>
