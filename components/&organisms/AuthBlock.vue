ebc12c0e061894cff6199864c4507e5373d9031a
Semenov Sergey <semenov@artw.ru>
2020-12-23 10:03:50 +0300

    Добавил auth блок

---
+++ components/&organisms/AuthBlock.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="auth">
+		<div class="auth__form">
+			<form action="">
+				<input type="text">
+				<input type="text">
+			</form>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/auth.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="container">
+		<AuthBlock/>
+	</div>
+</template>
+
+<script>
+import AuthBlock from '&/AuthBlock';
+
+export default {
+	components: {
+		AuthBlock,
+	}
+}
+</script>
+
+<style>
+
+</style>
