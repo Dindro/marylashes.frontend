069eba535fe4596e0f6d7b635e1b0e0bc89e4f0a
Semenov Sergey <semenov@artw.ru>
2021-02-02 00:24:00 +0300

    Добавил структуру для записи

--- components/^molecules/CardTileRecord.vue
+++ components/^molecules/CardTileRecord.vue
@@ -7,7 +7,39 @@
 		</template>
 
 		<template #default>
+			<div class="tile-record-row">
+				<p class="tile-record-row__content">
+					Ижуткина Мария
+					<span>(была раннее)</span>
+				</p>
+			</div>
 
+			<div class="tile-record-row">
+				<p class="tile-record-row__title">Услуги</p>
+				<p class="tile-record-row__content">Классика и снятие</p>
+			</div>
+
+			<div class="tile-record-row">
+				<p class="tile-record-row__title">Дата и время</p>
+				<p class="tile-record-row__content">13.10.2020 15:00</p>
+			</div>
+
+			<div class="tile-record-row">
+				<p class="tile-record-row__title">Социальные сети</p>
+				<table class="tile-record-row__content">
+					<tbody>
+						<tr>
+							<th>Vk</th>
+							<td>i7enov</td>
+						</tr>
+
+						<tr>
+							<th>In</th>
+							<td>i7enov</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</template>
 
 		<template #footer>
@@ -34,7 +66,7 @@ export default {
 					id: 'create',
 					name: 'Ижуткина Мария',
 					was: true,
-					services: [],
+					services: [0, 2, 4],
 					date: 'Помощь',
 					socials: [
 
@@ -42,12 +74,47 @@ export default {
 				}
 			}
 		},
-	}
+	},
 }
 </script>
 
 <style lang="scss">
-.card-tile {
+.tile-record {
 
+}
+
+.tile-record-row {
+	$b: #{&};
+
+	@include text-default;
+
+	& + & {
+		margin-top: rem(16);
+	}
+
+	&__title {
+		opacity: 0.3;
+	}
+
+	table#{$b}__content {
+		@include text-default;
+
+		th,
+		td {
+			padding: 0;
+		}
+
+		td {
+			padding: 0 rem(16);
+
+			&:last-child {
+				padding-right: 0;
+			}
+		}
+
+		th {
+			opacity: 0.3;
+		}
+	}
 }
 </style>
